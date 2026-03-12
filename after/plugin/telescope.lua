d89a362502bcd5e98bd219ae1ca4ba2909517def
xvitxr <vitor.mon1.com@gmail.com>
2026-03-12 16:27:55 -0300

    idk why theres so many changes but ok ig

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,5 @@ local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>p", vim.cmd.Telescope, {})
+
+vim.keymap.set("n", "<leader>e", builtin.buffers, {})
--- lua/xvi/remaps.lua
+++ lua/xvi/remaps.lua
@@ -1,10 +1,13 @@
 -- netrw
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>E", vim.cmd.Ex)
 
 -- clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- :w
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
 
 -- yanked from the primeagen
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
