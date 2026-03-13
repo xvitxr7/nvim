0c46fc6d2bc7de0785d9c19d097963656957119b
xvitxr <vitor.mon1.com@gmail.com>
2026-03-13 00:22:42 -0300

    new bind

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,4 @@ vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>p", vim.cmd.Telescope, {})
 
 vim.keymap.set("n", "<leader>e", builtin.buffers, {})
+vim.keymap.set("n", "<leader>t", builtin.lsp_dynamic_workspace_symbols, {})
